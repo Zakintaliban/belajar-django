e46681d1b7d97e1f247465154d3c43ee916446b2
Khasbullah Nukman Zakin <hasbizakin0804@gmail.com>
2023-08-04 23:42:05 +0700

    add test

--- belajar/myapp/tests.py
+++ belajar/myapp/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import MovieList, GenreChoices
 
 # Create your tests here.
+
+
+class MovieListTestCase(TestCase):
+    def setUp(self):
+        MovieList.objects.create(
+            title="Test Movie", genre=GenreChoices.ACTION, year=2022
+        )
+
+    def test_movie_list_created(self):
+        """MovieList objects are saved properly"""
+        movie = MovieList.objects.get(title="Test Movie")
+        self.assertEqual(movie.genre, GenreChoices.ACTION)
+        self.assertEqual(movie.year, 2022)
